92743c3cbca37858164e94b1bddd26bd7533287f
Dalio0031 <sam.dan_97@mail.ru>
2025-08-12 15:26:12 +0300

    задача 1

---
+++ multithreading/src/Main.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.*;
+
+public class Main {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        String[] texts = new String[25];
+        for (int i = 0; i < texts.length; i++) {
+            texts[i] = generateText("aab", 30_000);
+        }
+        long startTs = System.currentTimeMillis();
+        final ExecutorService executor = Executors.newFixedThreadPool(
+                Runtime.getRuntime().availableProcessors()
+        );
+
+        List<Future<Integer>> futures = new ArrayList<>();
+
+        for (String text : texts) {
+            Callable<Integer> callable = () -> {
+                int maxSize = 0;
+                for (int i = 0; i < text.length(); i++) {
+                    for (int j = 0; j < text.length(); j++) {
+                        if (i >= j) {
+                            continue;
+                        }
+                        boolean bFound = false;
+                        for (int k = i; k < j; k++) {
+                            if (text.charAt(k) == 'b') {
+                                bFound = true;
+                                break;
+                            }
+                        }
+                        if (!bFound && maxSize < j - i) {
+                            maxSize = j - i;
+                        }
+                    }
+                }
+                return maxSize;
+            };
+            futures.add(executor.submit(callable));
+        }
+
+        executor.shutdown();
+
+        int overallMax = 0;
+        for (Future<Integer> future : futures) {
+            int result = future.get();
+            if (result > overallMax) {
+                overallMax = result;
+            }
+        }
+
+        long endTs = System.currentTimeMillis();
+
+        System.out.println("Overall max interval: " + overallMax);
+        System.out.println("Time: " + (endTs - startTs) + "ms");
+    }
+
+    public static String generateText(String letters, int length) {
+        Random random = new Random();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            text.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return text.toString();
+    }
+}
